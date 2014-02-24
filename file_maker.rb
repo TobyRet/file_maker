38e1aca322a4209a0d1d3e0b66e337de735e374d
Toby Retallick <toby.retallick@gmail.com>
2014-02-24 16:28:54 +0000

    Converted switch statement to methods

--- file_maker.rb
+++ file_maker.rb
@@ -5,34 +5,66 @@
 #If the user types 'introspect' make the program print itself to the screen
 
 
-def make_file(file_name)
-  file=File.new(file_name, 'w')
+# get text from user
+def add_input
+  puts "Welcome to the file maker"
+  puts "Please enter the text you would like to add to the file"
+  input = gets.chomp
 end
 
+# display menu options. Record user option.
 def menu
   puts "Select an option from the menu"
   puts "------------------------------"
   puts "1. Save"
   puts "2. Read"
   puts "3. Introspect"
-  user_input(file)
+  response = gets.chomp
 end
 
-def user_input(file)
-  response = gets.chomp
-
-  case
-  when response == 1.to_s
-    puts "Save selected"
-  when response == 2.to_s
-    puts "Read selected"
-  when response == 3.to_s
-    puts "Introspect selected"
+# redirect user to correct method based on option chosen.
+def decision(user_choice, file_name = 'hello_from_ruby.txt', user_input)
+  if user_choice == 1.to_s
+    save(file_name, user_input)
+  elsif user_choice == 2.to_s
+    read
+  elsif user_choice == 3.to_s
+    introspect
   else
     puts "That is not an option, fool. Try again. Did you enter a number?"
     menu
   end
 end
 
-make_file('hello_from_ruby.txt')
-menu
+# save option
+def save(file_name, user_input)
+  puts "Save selected"
+  file = File.new(file_name, 'w')
+  File.open(file_name, 'a') do |file|
+    file.puts user_input
+      puts "Content saved to '#{file_name}'."
+    end
+  menu
+end
+
+# read option
+def read
+  puts "Read selected"
+  File.open(file_name, 'r') do |file|
+    file.each_line do |line|
+      puts "#{line}"
+    end
+  end
+  menu
+end
+
+# introspect option
+def introspect
+  puts "Introspect selected"
+end
+
+file_name = 'hello_from_ruby.txt'
+user_input = add_input
+user_choice = menu
+decision(user_choice,file_name, user_input)
+
